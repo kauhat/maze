ce86546057e5d18a145d037711b63ccbff3e517b
Jack Fletcher <jackowenfletcher@gmail.com>
2022-02-03 23:48:19 +0000

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { Maze, MazeConfig } from "./maze";
+import p5 from "p5";
+// import "p5/lib/addons/p5.dom";
+
+import { Maze } from "./maze";
+
+// p5.js sketches
 import { linesSketch } from "./sketches/lines";
 import { blocksSketch } from "./sketches/blocks";
 import { borderlessBlocksSketch } from "./sketches/borderless-blocks";
 
-const mazeConfig: MazeConfig = {
+/**
+ * Set up a global maze instance so we can compare drawing methods.
+ */
+const defaultMaze = new Maze({
   width: 520,
   height: 100,
 
@@ -13,21 +21,36 @@ const mazeConfig: MazeConfig = {
   directionWeights: [0.8, 0.8, 0.8, 1.0],
   gap: 3,
   border: 3
-};
+});
 
+// Run path creation algorithm.
+defaultMaze.findPath();
+
+/**
+ * Make a public function to get the instance from elsewhere.
+ */
 export function globalMaze() {
-  // Setup maze...
-  const maze: Maze = new Maze(mazeConfig);
-  maze.findPath();
-
-  return maze;
+  return defaultMaze;
 }
 
 /**
+ * Draw the sketches on page when loaded.
+ *
  * Here we're using P5 in instance mode.
  *
  * @see https://github.com/processing/p5.js/wiki/Global-and-instance-mode
  */
-new p5(linesSketch, document.getElementById("lines")!);
-new p5(blocksSketch, document.getElementById("blocks")!);
-new p5(borderlessBlocksSketch, document.getElementById("moreBlocks")!);
+export function setup() {
+  console.log("Setting up p5 sketches...");
+  new p5(linesSketch, document.getElementById("lines")!);
+  new p5(blocksSketch, document.getElementById("blocks")!);
+  new p5(borderlessBlocksSketch, document.getElementById("moreBlocks")!);
+}
+
+// Run setup on load.
+// window.addEventListener("load", (event) => {
+//   setup();
+// });
+
+// CodeSandbox doesn't like window load events.
+setup();
